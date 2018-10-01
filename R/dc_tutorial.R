b41736581c7af6816da50e63adb3f6b187f1073d
alexandraback <alexandra1.back@gmail.com>
2018-10-01 22:43:09 +0200

    R-exempel

--- R/dc_tutorial.R
+++ R/dc_tutorial.R
@@ -1,19 +1,13 @@
 #install.packages("tree",repos = "https://CRAN.R-project.org/")
 library(tree)
 
-weather_data <- read.csv("weather.csv");
+weather_data <- read.csv("../data/weather.csv");
 attribute_names <- names(weather_data)
 
 print(str(weather_data))
 
-#Print attribute names
-print(attribute_names)
-
-#Print dimensions of dataset
-print(dim(weather_data))
-
 #SPLIT DATASET INTO TRAIN SET AND TEST SET
-#set.seed(101)
+#set.seed(101) <- før att replikera sample
 train = sample(1:nrow(weather_data),10)
 
 weather_data.train = weather_data[train,]
@@ -22,7 +16,6 @@ weather_data.test = weather_data[-train,]
 #TRÆNA TRÆDET
 tree.weather = tree(play~.,data=weather_data, subset=train)
 
-
 summary(tree.weather)
 plot(tree.weather)
 text(tree.weather,pretty=0)
@@ -30,3 +23,6 @@ text(tree.weather,pretty=0)
 
 #PREDIKTERA
 tree.pred = predict(tree.weather, weather_data.test, type="class")
+
+#SUM THE DIAGONALS / NUMBER OF DATA_ENTRIES TO GET ERROR
+with(weather_data.test, table(tree.pred, play))
